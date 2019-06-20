c73ad514f61e3964a03aedb9d1ae6f4ca0e313cb
Ransom Maeda <random.maeda@gmail.com>
2019-06-20 16:37:36 -0700

    initial commit

--- 2002807_Maeda_MailOrder11/Form1.Designer.cs
+++ 2002807_Maeda_MailOrder11/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.catalogList = new System.Windows.Forms.ComboBox();
+            this.labelCatalogList = new System.Windows.Forms.Label();
+            this.addCatalog = new System.Windows.Forms.Button();
+            this.deleteCatalog = new System.Windows.Forms.Button();
+            this.catalogAmount = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // catalogList
+            // 
+            this.catalogList.FormattingEnabled = true;
+            this.catalogList.Location = new System.Drawing.Point(80, 17);
+            this.catalogList.Name = "catalogList";
+            this.catalogList.Size = new System.Drawing.Size(121, 21);
+            this.catalogList.TabIndex = 0;
+            // 
+            // labelCatalogList
+            // 
+            this.labelCatalogList.AutoSize = true;
+            this.labelCatalogList.Location = new System.Drawing.Point(12, 20);
+            this.labelCatalogList.Name = "labelCatalogList";
+            this.labelCatalogList.Size = new System.Drawing.Size(62, 13);
+            this.labelCatalogList.TabIndex = 1;
+            this.labelCatalogList.Text = "Catalog List";
+            // 
+            // addCatalog
+            // 
+            this.addCatalog.Location = new System.Drawing.Point(277, 20);
+            this.addCatalog.Name = "addCatalog";
+            this.addCatalog.Size = new System.Drawing.Size(158, 29);
+            this.addCatalog.TabIndex = 2;
+            this.addCatalog.Text = "Add Catalog";
+            this.addCatalog.UseVisualStyleBackColor = true;
+            this.addCatalog.Click += new System.EventHandler(this.addCatalog_Click);
+            // 
+            // deleteCatalog
+            // 
+            this.deleteCatalog.Location = new System.Drawing.Point(277, 55);
+            this.deleteCatalog.Name = "deleteCatalog";
+            this.deleteCatalog.Size = new System.Drawing.Size(158, 29);
+            this.deleteCatalog.TabIndex = 3;
+            this.deleteCatalog.Text = "Delete Catalog";
+            this.deleteCatalog.UseVisualStyleBackColor = true;
+            this.deleteCatalog.Click += new System.EventHandler(this.deleteCatalog_Click);
+            // 
+            // catalogAmount
+            // 
+            this.catalogAmount.Location = new System.Drawing.Point(277, 90);
+            this.catalogAmount.Name = "catalogAmount";
+            this.catalogAmount.Size = new System.Drawing.Size(158, 29);
+            this.catalogAmount.TabIndex = 4;
+            this.catalogAmount.Text = "Amount of Catalogs";
+            this.catalogAmount.UseVisualStyleBackColor = true;
+            this.catalogAmount.Click += new System.EventHandler(this.catalogAmount_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(277, 125);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(158, 29);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "Clear Catalog List";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(277, 160);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(158, 29);
+            this.save.TabIndex = 6;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(55, 160);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(158, 29);
+            this.exit.TabIndex = 7;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(459, 207);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.catalogAmount);
+            this.Controls.Add(this.deleteCatalog);
+            this.Controls.Add(this.addCatalog);
+            this.Controls.Add(this.labelCatalogList);
+            this.Controls.Add(this.catalogList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox catalogList;
+        private System.Windows.Forms.Label labelCatalogList;
+        private System.Windows.Forms.Button addCatalog;
+        private System.Windows.Forms.Button deleteCatalog;
+        private System.Windows.Forms.Button catalogAmount;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button exit;
     }
 }
 
--- 2002807_Maeda_MailOrder11/Form1.cs
+++ 2002807_Maeda_MailOrder11/Form1.cs
@@ -6,15 +6,154 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace _2002807_Maeda_MailOrder11
 {
     public partial class Form1 : Form
     {
+        public bool isChange = false;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            //saves contents to a file
+            StreamWriter catalogStreamWriter = new StreamWriter("Catalogs.txt");
+            int indexInteger, maximumInteger;
+
+            maximumInteger = catalogList.Items.Count;
+            for (indexInteger = 0; indexInteger < maximumInteger; indexInteger++)
+            {
+                catalogStreamWriter.WriteLine(catalogList.Items[indexInteger]);
+            }
+            catalogStreamWriter.Close();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            //clears combo box
+            DialogResult confirmDialogResult = MessageBox.Show("Remove all catalogs?",
+"Clear Catalog List", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmDialogResult == DialogResult.Yes)
+            {
+                catalogList.Items.Clear();
+                catalogList.Text = null;
+                isChange = true;
+            }
+
+        }
+
+        private void catalogAmount_Click(object sender, EventArgs e)
+        {
+           //displays amount of catalogs
+           MessageBox.Show("Catalog Count: " +
+           catalogList.Items.Count.ToString(),
+           "Mail Order", MessageBoxButtons.OK,
+           MessageBoxIcon.Information);
+        }
+
+        private void deleteCatalog_Click(object sender, EventArgs e)
+        {
+            if (catalogList.SelectedIndex != -1)
+            {
+                //remove selected catalog
+                catalogList.Items.RemoveAt(catalogList.SelectedIndex);
+                isChange = true;
+
+            }
+            else
+            {
+                //if no catalog selected
+                MessageBox.Show("Select a catalog to remove.", "No Selection",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+        }
+
+        private void addCatalog_Click(object sender, EventArgs e)
+        {
+            //adds new catalog
+            int indexInteger = 0;
+            bool itemFoundBoolean = false;
+            //Checks for duplicates
+            if (catalogList.Text != string.Empty)
+            {
+                while (indexInteger < catalogList.Items.Count &&
+                !itemFoundBoolean)
+                {
+                    if (catalogList.Items[indexInteger++].ToString().ToUpper()
+                    == catalogList.Text.ToUpper())
+                    {
+                        MessageBox.Show("Duplicate catalog.", "Catalog Not Added",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        itemFoundBoolean = true;
+                    }
+                }
+                if (!itemFoundBoolean)
+                {
+                    // Adds to list.
+                    catalogList.Items.Add(catalogList.Text);
+                    isChange = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter new catalog",
+                "No catalog Entered", MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            //prompts user to save file
+            DialogResult responseDialogResult;
+
+            if (isChange)
+            {
+                responseDialogResult = MessageBox.Show("Catalog list has changed. Save the list?", "Catalog List Changed",
+                  MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (responseDialogResult == DialogResult.Yes)
+                {
+                    save_Click(sender, new System.EventArgs());
+                }
+            }
+                //closes the application
+                this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load the combo box
+            DialogResult responseDialogResult;
+            try
+            {
+                StreamReader catalogStreamReader = new StreamReader("Catalogs.txt");
+                while (catalogStreamReader.Peek() != -1)
+                {
+                    catalogList.Items.Add(catalogStreamReader.ReadLine());
+                }
+                catalogStreamReader.Close();
+            }
+            catch
+            {
+                responseDialogResult = MessageBox.Show("Create a new file?", "File not Found", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (responseDialogResult == DialogResult.No)
+                {
+                    //exit the project
+                    exit_Click(e, new System.EventArgs());
+                }
+            }
+        }
+
+
     }
 }
+
+
